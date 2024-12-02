663e094ccf1cb6c0d6dd89b05989a98112abc11a
Junoiri <martasadlowska77@gmail.com>
2024-12-02 23:48:23 +0100

    Fixed visibility issue in the HomeScreen

--- app/src/main/java/com/example/pomodojo/functionality/dashboard/screens/HomeScreen.kt
+++ app/src/main/java/com/example/pomodojo/functionality/dashboard/screens/HomeScreen.kt
@@ -1,35 +1,26 @@
 package com.example.pomodojo.functionality.dashboard.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.pomodojo.ui.theme.PomodojoTheme
-import com.example.pomodojo.functionality.dashboard.viewmodel.HomeViewModel
-import androidx.compose.foundation.background
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.runtime.*
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import com.example.pomodojo.R
+import com.example.pomodojo.functionality.dashboard.viewmodel.HomeViewModel
+import com.example.pomodojo.ui.theme.PomodojoTheme
 
 /**
  * Composable function that displays the main screen of the application.
@@ -50,7 +41,8 @@ fun MainScreen(
                 .fillMaxSize()
                 .background(backgroundColor)
                 .padding(padding)
-                .padding(16.dp),
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(modifier = Modifier.height(16.dp))
